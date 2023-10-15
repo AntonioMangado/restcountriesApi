c69ddead3cc7c1baaf4bcfb999f61be1c4a56c3f
AntonioMangado <antonio.mangado@gmail.com>
2023-10-15 16:08:38 +0400

    ADD/ all countries on landing

--- index.js
+++ index.js
@@ -10,11 +10,21 @@
 const countriesNode = document.getElementById("countries");
 const form = document.getElementById("continent-filter-form");
 
+fetch("https://restcountries.com/v3.1/all")
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.length; i++) {
+
+    let card = `<div class="card">
+                  <img id="flag-image" src="${data[i].flags.png}" alt="flag" />
+                  <h1 class="center">${data[i].name.common}</h1>
+                </div>`
+
+    countriesNode.innerHTML += card;           
+    }
+  })
 
 form.addEventListener("submit", function (event){
-  
-  // console.log(event);
-  
   event.preventDefault();
   countriesNode.innerHTML = ""; 
 
